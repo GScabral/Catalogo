6ca411b1f70db176c971e301cc1fe02757f6c1a0
GScabral <126419509+GScabral@users.noreply.github.com>
2025-04-27 23:19:43 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import reactSWC from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
+ plugins: [reactSWC()],
   server: {
     host: '0.0.0.0', // Escucha en todas las interfaces
     port: 3005,     // El puerto en el que tu aplicación está sirviendo (coincide con el log)
