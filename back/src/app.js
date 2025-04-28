5a1f2530a48108d63685370c3db1b16b838c1c26
GsCabral <cabralgonzalo10@gmail.com>
2025-04-27 22:19:41 -0300

    cambiobd

--- back/src/app.js
+++ back/src/app.js
@@ -18,7 +18,7 @@ server.use(bodyParser.json({limit:'50mb'}));
 server.use(cookieParser());
 
 server.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "http://localhost:3005");
+    res.header("Access-Control-Allow-Origin", "https://amore-mio.onrender.com");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS,PATCH");
